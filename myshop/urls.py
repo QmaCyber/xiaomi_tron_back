bf2340d887f5ed12755152c02c73f259b676b438
Dolphin-web <im.zhaslan@gmail.com>
2021-04-02 18:01:57 +0600

    add new apps

--- myshop/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from shop import views
-
-urlpatterns = [
-	path('admin/', admin.site.urls),
-	path('api/', include('shop.urls')),
-]
--- shop/urls.py
+++ shop/urls.py
@@ -1,19 +1,21 @@
+"""shop URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
 from django.urls import path
-from . import views
 
-app_name = 'api'
 urlpatterns = [
-	path('products/', views.ProductsView.as_view()),
-	path('products/<str:categorySlug>', views.ProductCategoryView.as_view()),
-	path('categorys', views.CategoryView.as_view()),
-	path('product/<str:productSlug>', views.ProductView.as_view()),
-	path('popularproducts/', views.PopularProductsView.as_view()),
-	path('sliders/', views.SlidersView.as_view()),
-	path('search/<str:text>', views.SearchView.as_view()),
-	path('news/', views.NewsView.as_view()),
-	path('news/<str:newsSlug>', views.NewsView.as_view()),
-	path('reviews/', views.ReviewsView.as_view()),
-	path('auth', views.LoginView.as_view()),
-	path('reg', views.ResiterView.as_view()),
+    path('admin/', admin.site.urls),
 ]
-
